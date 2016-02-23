dcc26efded1f3399c4d807be0b3b95d9129ad418
Tin Pavlinic <tin.pavlinic@gmail.com>
2016-02-23 15:41:43 +1100

    First shot at implementation

--- lib/Tablinator/Table.hs
+++ lib/Tablinator/Table.hs
@@ -4,10 +4,10 @@ module Tablinator.Table
     processObjectStream
 ) where
 
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Text (Text)
-import Text.Pandoc (Pandoc, Block)
+import Data.List (sort)
+import Data.Map.Strict (Map, keys, assocs)
+import Data.Text (Text, unpack)
+import Text.Pandoc
 
 --
 -- | A table column. You need to specify an Ord instance which will determine
@@ -17,10 +17,8 @@ import Text.Pandoc (Pandoc, Block)
 class Ord a => Column a where
     heading :: a -> Text
 
-
-kvPairsToTable :: [(String,String)] -> Block
-kvPairsToTable _ = undefined
-
+headings :: Column k => Map k a -> [Text]
+headings m = fmap heading $ sort $ keys m
 
 --
 -- | Given a stream (at present modelled as a list) of input data objects (each
@@ -28,6 +26,19 @@ kvPairsToTable _ = undefined
 -- for subsequent emplacement in a Pandoc document.
 --
 processObjectStream :: Column k => [Map k Text] -> [Block]
-processObjectStream = undefined
-
-
+processObjectStream mps = let
+  hdings  = unpack <$> headings (head mps)
+  inline  = [Str "This is the caption"]
+  align   = const AlignLeft <$> hdings
+  widths  = const 0 <$> hdings
+  mkHeader :: String -> [Block]
+  mkHeader h = [Plain [Str h]]
+  headers = mkHeader <$> hdings
+  mkRow :: Map k Text -> [[Block]]
+  mkRow mp = (\(_,v) -> [Para [Str $ unpack v]]) <$> assocs mp
+  rows = fmap mkRow mps
+  --row1    = [[Para [Str "Left"]], [Para [Str "Right"]]]
+  --row2    = [[Para [Str "And then"]],
+  --           [Para [Code nullAttr "printf(\"Hello World\\n\");"]]]
+    in
+      [Table inline align widths headers rows]
